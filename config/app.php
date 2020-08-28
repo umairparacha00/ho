67fc090c7214f3b69b8dda5008462621c22a0b4c
umairparacha00 <umairparacha0047@gmail.com>
2020-08-28 16:44:03 +0500

    haskd45

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+		App\Providers\HorizonServiceProvider::class,
 
         /*
          * Package Service Providers...
