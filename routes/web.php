2b77afc18eb5c63b35a0ebefe46d90e080ce7722
umairparacha00 <umairparacha0047@gmail.com>
2020-08-28 16:40:45 +0500

    haskd

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,26 @@
 	use App\User;
 	use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
-	Route::get('/umair', function () {
-		User::find(1)->notify( new umair());
-		return 'done';
+	Route::get('/', function () {
+		return view('welcome');
 	});
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+	Auth::routes();
+	Route::middleware('auth')->group(function () {
+		Route::get('/home', 'HomeController@index')->name('home');
+		Route::get('/umair', function () {
+			User::find(1)->notify(new umair());
+			return 'done';
+		});
+	});
